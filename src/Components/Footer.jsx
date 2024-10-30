ea0f44ba9a9666ecd262d2e311e83cefc3255f3d
Varun Pandey <varunp5324@gmail.com>
2024-10-30 16:46:24 +0530

    project

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,34 +4,36 @@ function Footer() {
 
   return (
     <>
-    <div className="flex flex-wrap bg-black px-5 w-full justify-evenly items-center pb-5 py-5" id="footer">
-      <div className="text-white px-3 sm:w-1/3 w-full">
-      <img src="https://i.ibb.co/1mrY8p4/Dimansn-Architects-final4-Photoroom-1-1.png" alt="Dimansn-Architects-final4-Photoroom-1" border="0" className="mx-auto mb-6"></img>
-      <p className="md:px-10 sm:px-5">Dimansn Architects is more than just a design firm; we create inspiring spaces. Led by Er. Bhargav Chauhan, we offer comprehensive architectural services, from concept to completion. Let's turn your dream projects into reality.</p>
+      <div className=" md:flex bg-black px-6 w-full justify-evenly items-center pb-5" id="footer">
+        <div className="text-white md:w-1/3 w-full px-7 md:pl-7 md:pr-0">
+          <img src="https://i.ibb.co/0GHDWM9/Dimansn-Architects-final4-Photoroom-1.png" alt="Dimansn-Architects-final4-Photoroom-1" border="0" className="mx-auto"></img>
+          <p>Dimansn Architects is more than just a design firm; we create inspiring spaces. Led by Er. Bhargav Chauhan, we offer comprehensive architectural services, from concept to completion. Let's turn your dream projects into reality.</p>
+        </div>
+        <div className="flex w-full justify-around items-center">
+          <div className=" py-5 w-1/4 sm:w-1/3 sm:text-center text-right">
+            <p className=" inter text-[18px] text-white font-bold"><a href="#Navbar">Home</a></p>
+            <br />
+            <p className=" inter text-[14px] text-gray-300"><a href="#AboutUs">About Us</a></p>
+            <br />
+            <p className=" inter text-[14px] text-gray-300"><a href="#Services">Services</a></p>
+            <br />
+            <p className=" inter text-[14px] text-gray-300"><a href="#HowWeWork">Our Work</a></p>
+            <br />
+            <p className=" inter text-[14px] text-gray-300"><a href="#Navbar">Contact Us</a></p>
+          </div>
+          <div className=" py-5 w-3/4 sm:w-1/3 text-center">
+            <p className=" inter text-[18px] text-white font-bold">Contact</p>
+            <br />
+            <p className=" inter text-[18px] text-white font-bold">Er. BHARGAV CHAUHAN</p>
+            <p className=" inter text-[14px] text-gray-300">Architect &amp; Project Consultant</p>
+            <br />
+            <p className=" inter text-[14px] text-gray-300">dimansnarchitects@gmail.com</p>
+            <br />
+            <p className=" inter text-[14px] text-gray-300">Our Work</p>
+            <p className=" inter text-[14px] text-gray-300">+91 99683 88888</p>
+          </div>
+        </div>
       </div>
-      <div className="px-1 py-5 w-1/4 sm:w-1/3 sm:text-center text-right">
-        <p className=" inter text-[18px] text-white font-bold"><a href="#Navbar">Home</a></p>
-        <br />
-        <p className=" inter text-[14px] text-gray-300"><a href="#AboutUs">About Us</a></p>
-        <br />
-        <p className=" inter text-[14px] text-gray-300"><a href="#Services">Services</a></p>
-        <br />
-        <p className=" inter text-[14px] text-gray-300"><a href="#HowWeWork">Our Work</a></p>
-        <br />
-        <p className=" inter text-[14px] text-gray-300"><a href="https://forms.gle/EsRy3n6h4WNoHP9U6">Contact Us</a></p>
-      </div>
-      <div className="px-3 py-5 w-3/4 sm:w-1/3 text-center">
-        <p className=" inter text-[18px] text-white font-bold">Contact</p>
-        <br />
-        <p className=" inter text-[18px] text-white font-bold">Er. BHARGAV CHAUHAN</p>
-        <p className=" inter text-[14px] text-gray-300">Architect &amp; Project Consultant</p>
-        <br />
-        <p className=" inter text-[14px] text-gray-300">dimansnarchitects@gmail.com</p>
-        <br />
-        <p className=" inter text-[14px] text-gray-300">Our Work</p>
-        <p className=" inter text-[14px] text-gray-300">+91 99683 88888</p>
-      </div>
-    </div>
     </>
   )
 }
--- src/Components/OurClient.jsx
+++ src/Components/OurClient.jsx
@@ -9,7 +9,7 @@ function OurClient() {
   return (
     <>
       <div className="flex justify-between flex-wrap my-20" id='testimonial'>
-        <div className="pl-20 md:w-1/2 pr-10 w-[95%]">
+        <div className=" md:w-1/2 px-5 w-full">
         <Testimonial></Testimonial>
         </div>
         <img src="https://i.ibb.co/bL2mY0p/Mask-group-2.png" alt="Mask-group-2" border="0" className="w-full md:w-1/2 flex justify-center"></img>
