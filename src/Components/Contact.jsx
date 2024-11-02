dd75dd11af102c70dd2a91070ca38c30d54f09d8
Varun Pandey <varunp5324@gmail.com>
2024-11-02 21:28:05 +0530

    project

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -70,12 +70,12 @@ function Contact() {
               </form>
             </>
             : <>
-              <div id="alert-1" class="flex items-center p-4 mb-4 text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+              <div id="alert-1" class="flex items-center p-4 mb-4 text-blue-800 rounded-lg bg-blue-50" role="alert">
                 <span class="sr-only">Info</span>
                 <div class="ms-3 text-sm font-medium">
                 Your message has been received. We appreciate your feedback and will be in touch shortly.
                 </div>
-                <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-blue-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-1" aria-label="Close" onClick={() => setDataSend(false)}>
+                <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex items-center justify-center h-8 w-8 " data-dismiss-target="#alert-1" aria-label="Close" onClick={() => setDataSend(false)}>
                   <span class="sr-only">Close</span>
                   <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
